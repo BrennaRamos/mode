65d70330090cf81d31336542d924a7088b74a8c1
Bramo033 <ramos.brenna.142@gmail.com>
2023-11-28 03:11:33 -0500

    Im done for now

---
+++ src/game_mod.rs
@@ -0,0 +1,263 @@
+use bevy::{core_pipeline::clear_color::ClearColorConfig, prelude::*, render::camera::ScalingMode};
+use chrono::*;
+use colored::Colorize;
+use rand::Rng;
+use std::{io, process::exit};
+
+use crate::AppState;
+
+#[derive(Resource, Default)]
+pub struct GameData {
+    player_guess: String,
+    green_tally: i32,
+    red_tally: i32,
+}
+
+pub fn game(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut next_state: ResMut<NextState<AppState>>,
+    interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, &mut BorderColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut game_data: ResMut<GameData>,
+) {
+    game_data.green_tally = 0;
+    game_data.red_tally = 0;
+    let mut level = 1;
+    let mut rng = rand::thread_rng();
+    let mut input = String::new();
+    // let (sender, receiver) = std::sync::mpsc::channel();
+
+    let title = format!("Level {} - Which color has more tallies?", level);
+    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+    let level_entity = commands.spawn({
+        Text2dBundle {
+            text: Text::from_section(
+                title,
+                TextStyle {
+                    font,
+                    font_size: 64.0,
+                    color: Color::WHITE,
+                },
+            )
+            .with_alignment(TextAlignment::Center),
+            transform: Transform::from_translation(Vec3::new(0.0, 60.0, 0.0)),
+            ..default()
+        }
+    });
+
+    for _iter in 3..rng.gen_range(4..20) {
+        let color = print_shapes(
+            rng.gen_range(0..10),
+            //rng.gen_range(0..10),
+            &asset_server,
+            &mut commands,
+            _iter,
+        );
+
+        if color == 'g' {
+            game_data.green_tally += 1;
+        } else {
+            game_data.red_tally += 1;
+        }
+    }
+
+    next_state.set(AppState::Pause);
+}
+
+fn print_shapes(
+    random: i32,
+    //print_amt: i32,
+    asset_server: &Res<AssetServer>,
+    commands: &mut Commands,
+    offset: i32,
+) -> (char) {
+    if random % 2 == 0 {
+        //for _iter in 0..1 {
+        let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+        let pop_entity = commands.spawn({
+            Text2dBundle {
+                text: Text::from_section(
+                    "X",
+                    TextStyle {
+                        font,
+                        font_size: 24.0,
+                        color: Color::GREEN,
+                    },
+                )
+                .with_alignment(TextAlignment::Center),
+                transform: Transform::from_translation(Vec3::new(
+                    0.0 + (offset * 20) as f32,
+                    0.0,
+                    0.0,
+                )),
+                ..default()
+            }
+        });
+        //}
+        return ('g');
+    } else {
+        //for _iter in 0..print_amt {
+        let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+        commands.spawn({
+            Text2dBundle {
+                text: Text::from_section(
+                    "X",
+                    TextStyle {
+                        font,
+                        font_size: 24.0,
+                        color: Color::RED,
+                    },
+                )
+                .with_alignment(TextAlignment::Center),
+                transform: Transform::from_translation(Vec3::new(
+                    0.0 + (offset * 20) as f32,
+                    0.0,
+                    0.0,
+                )),
+                ..default()
+            }
+        });
+        //}
+        return ('r');
+    }
+}
+
+pub fn clear_shapes(mut commands: Commands, mut query: Query<Entity, With<Text>>) {
+    for entity in query.iter_mut() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn process_guess(guess: String, green: i32, red: i32) -> bool {
+    if guess.trim() == "g" && green > red {
+        println!("{} Correct!\n", "✅".green());
+        return true;
+    } else if guess.trim() == "r" && red > green {
+        println!("{} Correct!\n", "✅".green());
+        return true;
+    } else {
+        println!("{} Incorrect!\n", "❌".red());
+        return false;
+    }
+}
+
+pub fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>) {
+    // Spawn Camera in Foreground
+    commands.spawn(Camera2dBundle {
+        camera_2d: Camera2d {
+            clear_color: ClearColorConfig::Custom(Color::BLACK),
+        },
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 5.0)),
+        ..default()
+    });
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                // bottom left button
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                justify_content: JustifyContent::Start,
+                align_items: AlignItems::End,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn(ButtonBundle {
+                style: Style {
+                    width: Val::Px(150.),
+                    height: Val::Px(65.),
+                    // horizontally center child text
+                    justify_content: JustifyContent::Center,
+                    // vertically center child text
+                    align_items: AlignItems::Center,
+                    border: UiRect {
+                        top: Val::Px(2.),
+                        left: Val::Px(2.),
+                        bottom: Val::Px(2.),
+                        right: Val::Px(2.),
+                    },
+                    ..default()
+                },
+                background_color: Color::GREEN.into(),
+                ..default()
+            });
+        });
+
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                // bottom right button
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                justify_content: JustifyContent::End,
+                align_items: AlignItems::End,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn(ButtonBundle {
+                style: Style {
+                    width: Val::Px(150.),
+                    height: Val::Px(65.),
+                    // horizontally center child text
+                    justify_content: JustifyContent::Center,
+                    // vertically center child text
+                    align_items: AlignItems::Center,
+                    border: UiRect {
+                        top: Val::Px(2.),
+                        left: Val::Px(2.),
+                        bottom: Val::Px(2.),
+                        right: Val::Px(2.),
+                    },
+                    ..default()
+                },
+                background_color: Color::RED.into(),
+                ..default()
+            });
+        });
+}
+
+pub fn game_over(mut commands: Commands, game_data: Res<GameData>) {
+    //exit game.
+}
+
+pub fn interact_button(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, &mut BorderColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut game_data: ResMut<GameData>,
+    mut next_state: ResMut<NextState<AppState>>,
+) {
+    // Buttons
+    for (interaction, mut color, mut border_color) in interaction_query.iter_mut() {
+        match *interaction {
+            Interaction::Pressed => {
+                *border_color = Color::WHITE.into();
+                // if the background color of the button is green, return 'g'
+                if color.0 == Color::GREEN {
+                    game_data.player_guess = "g".to_string();
+                } else {
+                    game_data.player_guess = "r".to_string();
+                }
+                process_guess(
+                    game_data.player_guess.clone(),
+                    game_data.green_tally,
+                    game_data.red_tally,
+                );
+                next_state.set(AppState::StartGame);
+            }
+            Interaction::Hovered => {
+                *border_color = Color::BLACK.into();
+            }
+            Interaction::None => {
+                *border_color = Color::WHITE.into();
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,31 @@
-use chrono::*;
-use colored::Colorize;
-use rand::Rng;
-use std::io;
+use bevy::prelude::*;
+mod game_mod;
+use game_mod::*;
 
 fn main() {
-    let mut level = 1;
-    let mut rng = rand::thread_rng();
-
-    while level < 100 {
-        let mut green = 0;
-        let mut red = 0;
-        let mut input = String::new();
-        let (sender, receiver) = std::sync::mpsc::channel();
-
-        println!("Level: {}\n", level);
-
-        for _iter in 0..10 {
-            let (color, _print_amt) = print_shapes(rng.gen_range(0..10), rng.gen_range(0..10));
-
-            if color == 'g' {
-                green += _print_amt;
-            } else {
-                red += _print_amt;
-            }
-        }
-
-        println!();
-        println!("Which color has more tallies? g/r: ");
-        // MAKE A 5 SECOND TIMER
-        std::thread::spawn(move || {
-            match io::stdin().read_line(&mut input) {
-                Ok(_n) => {}
-                Err(error) => println!("error: {error}"),
-            }
-            sender.send(input.to_string()).unwrap();
-        });
-
-        match receiver.recv_timeout(Duration::seconds(5).to_std().unwrap()) {
-            Ok(input) => {
-                if process_guess(input, green, red) {
-                    level += 1;
-                } else {
-                    println!("Game Over");
-                    return;
-                }
-            }
-            Err(_error) => {
-                println!("{} Too slow!\n", "❌".red());
-                return;
-            }
-        }
-    }
-
-    println!("You Win!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .init_resource::<GameData>()
+        .add_systems(Startup, startup)
+        .add_state::<AppState>()
+        .add_systems(
+            OnEnter(AppState::StartGame),
+            (game_mod::clear_shapes, game_mod::game).chain(),
+        )
+        .add_systems(OnExit(AppState::GameOver), game_mod::game_over)
+        .add_systems(Update, game_mod::interact_button)
+        .run();
 }
 
-fn print_shapes(random: i32, print_amt: i32) -> (char, i32) {
-    if random % 2 == 0 {
-        for _iter in 0..print_amt {
-            print!("{}", "x ".green());
-        }
-        return ('g', print_amt);
-    } else {
-        for _iter in 0..print_amt {
-            print!("{}", "x ".red());
-        }
-        return ('r', print_amt);
-    }
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    setup_ui(&mut commands, &asset_server);
 }
 
-fn process_guess(guess: String, green: i32, red: i32) -> bool {
-    if guess.trim() == "g" && green > red {
-        println!("{} Correct!\n", "✅".green());
-        return true;
-    } else if guess.trim() == "r" && red > green {
-        println!("{} Correct!\n", "✅".green());
-        return true;
-    } else {
-        println!("{} Incorrect!\n", "❌".red());
-        return false;
-    }
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum AppState {
+    #[default]
+    StartGame,
+    Pause,
+    CheckAnswer,
+    GameOver,
 }
